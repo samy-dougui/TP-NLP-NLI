3dc39453e961aefa3066be3230d59bb167c361bc
gaspard <gaspard.blanchet@supelec.Fr>
2021-03-31 11:06:18 +0200

    addition of test_step in model.py

--- model.py
+++ model.py
@@ -1,7 +1,10 @@
 import torch
 import pytorch_lightning as pl
 
+from sklearn.metrics import f1_score
+
 from transformers import DistilBertForSequenceClassification
+from transformers import get_linear_schedule_with_warmup
 
 
 class SentencesClassification(pl.LightningModule):
@@ -17,7 +20,8 @@ class SentencesClassification(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
+        scheduler = get_linear_schedule_with_warmup(optimizer, 5, 2)
+        return [optimizer], [scheduler]
 
     def training_step(self, train_batch, batch_idx):
         inputs = {
@@ -42,4 +46,37 @@ class SentencesClassification(pl.LightningModule):
             inputs["input_ids"], inputs["attention_mask"], labels=inputs["labels"]
         )
         loss = outputs.loss
-        self.log("val_loss", loss)
+
+        predictions = torch.argmax(outputs, dim=1)
+        predictions.detach().cpu()
+        f1_acc = f1_score(
+            inputs["labels"].detach().cpu(),
+            predictions.detach().cpu(),
+            average="weighted",
+        )
+        metrics = {"val_f1_acc": f1_acc, "val_loss": loss}
+        self.log(metrics)
+        return metrics
+
+    def test_step(self, batch, batch_idx):
+        metrics = self.validation_step(batch, batch_idx)
+        metrics = {
+            "test_f1_acc": metrics["val_f1_acc"],
+            "test_loss": metrics["val_loss"],
+            "batch_size": len(batch),
+        }
+        self.log_dict(metrics)
+        return metrics
+
+    def test_epoch_end(self, output_results):
+        global_f1 = 0
+        global_loss = 0
+        for result in output_results:
+            global_f1 = result["val_f1_acc"] * result["batch_size"]
+            global_loss = result["test_loss"] * result["batch_size"]
+        metrics = {"final_test_f1_acc": global_f1, "final_test_loss": global_loss}
+        self.log(metrics)
+
+    def setup(self, stage="fit"):
+        for param in self.model.base_model.parameters():
+            param.requires_grad = False
